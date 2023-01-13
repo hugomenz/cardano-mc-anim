6f96927c197aa90401249059b299814b4a644074
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-13 13:17:23 +0100

    First approach header with real data

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { defaultIfEmpty, Subscription } from 'rxjs';
+import { Data } from 'src/app/core/model/data.model';
 import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
@@ -7,11 +9,37 @@ import { DataService } from 'src/app/core/services/data.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  data$: any;
+  firstData: any;
+  data: any;
+  subscription!: Subscription;
 
-  constructor(private dataService: DataService) {
-    this.data$ = this.dataService.getData();
+  constructor(public dataService: DataService) {}
+
+  ngOnInit() {
+    this.subscription = this.dataService.getData().subscribe((data) => {
+      this.firstData = data[0];
+      console.log(this.firstData);
+      this.data = data;
+    });
   }
 
-  ngOnInit(): void {}
+  next() {
+    this.dataService.next();
+    this.data = this.dataService.data[this.dataService.index];
+    console.log(this.data);
+  }
+
+  unsubscribe() {
+    this.data = this.firstData;
+    this.subscription.unsubscribe();
+    this.dataService.index = 0;
+    this.subscription = this.dataService
+      .getData()
+      .pipe(defaultIfEmpty([]))
+      .subscribe((data) => {
+        if (data.length > 0) {
+          this.data = data[this.dataService.index];
+        }
+      });
+  }
 }
